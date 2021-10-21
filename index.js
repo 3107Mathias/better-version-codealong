37330701ee892be39e1c49540ad3d9e591b45afc
Mathias Burger <92169466+3107Mathias@users.noreply.github.com>
2021-10-21 19:30:45 +0200

    run exercise committing on GitHub

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // matty message added to this file
 // matty's second comment
-// and another message to this file by matty
 // in total the fourth comment
+// and another message to this file by matty
+// matty added a new line 5, added line 3 to 4 and an empty line
