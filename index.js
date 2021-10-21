da40c636bd53b77e3a1b5f391ad5ca8f585c23a4
Mathias Burger <mathias.burger89@gmail.com>
2021-10-21 20:37:15 +0200

    adding the very last thing of tonights session 4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { compareAsc, format } from 'date-fns';
 // matty message added to this file
 // matty's second comment
 // in total the fourth comment
@@ -5,3 +6,22 @@
 // matty added a new line 5, added line 3 to 4 and an empty line
 // adding one more change via github
 // Lena's going wild, adding line 7
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//=> '2014-02-11'
+
+const dates = [
+  new Date(2021, 10, 21),
+  new Date(1987, 1, 11),
+  new Date(1989, 7, 26),
+]
+dates.sort(compareAsc)
+//=> [
+//   Wed Mar 31 1987 00:00:00,
+//   Mon Jul 31 1989 00:00:00,
+//   Thu Oct 21 2021 00:00:00
+// ]
+console.log('These are the dates:', dates);
